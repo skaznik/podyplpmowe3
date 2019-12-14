4c42be2789a40b8c78112d588cecfa69686cbe24
skaznik <skaznik@wszib.edu.pl>
2019-12-14 09:20:28 +0100

    zjazd 3

---
+++ src/RangeLister.java
@@ -0,0 +1,12 @@
+public class RangeLister {
+    int[] makeRange(int lower, int upper) { //metoda
+
+        int[] range = new int[(upper - lower)+1];
+        return range;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
