4cab08e18909ab792e16dc55190e9b029d8b4945
skaznik <skaznik@wszib.edu.pl>
2019-12-14 11:59:29 +0100

    zjazd 3

---
+++ src/przeciążanie_metod/Adder.java
@@ -0,0 +1,16 @@
+package przeciążanie_metod;
+
+public class Adder {
+    public static void main(String[] args) {
+        System.out.println(sum(1,2));
+        System.out.println(sum(3,4,5));
+    }
+
+    static int sum(int a, int b) {
+        return a+b;
+    }
+
+    static int sum(int a, int b, int c) {
+        return a+b+c;
+    }
+}
