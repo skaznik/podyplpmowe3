f95b307a0462ab66392251af4f2c5d65eaf2724c
skaznik <skaznik@wszib.edu.pl>
2019-12-14 09:57:31 +0100

    zjazd 3

--- src/Passing.java
+++ src/Passing.java
@@ -1,12 +1,24 @@
+import java.awt.*;
+
 public class Passing {
     public static void main(String[] args) {
         int i = 10;
         System.out.println("Przed zmianą ..." + i);
         changeIt(i);
         System.out.println("Po zmianie ..." + i);
+        Point p = new Point(1,1);
+        System.out.println("Przed zmianą p ..." + p);
+        changeIt(p);
+        System.out.println("Po zmianie ...." + p);
     }
     static void changeIt(int i) {
         System.out.println("Zmieniam i ...");
         i++;
+    }
+    static void changeIt(Point p) {
+        p = new Point(4,4);
+        System.out.println("Zmieniam to na co wskazuje p");
+        p.x++;
+        p.y++;
     }
 }
