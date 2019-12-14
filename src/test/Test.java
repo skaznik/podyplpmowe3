98f2b26756c30e15151a724f02bb701e284471b4
skaznik <skaznik@wszib.edu.pl>
2019-12-14 12:15:20 +0100

    zjazd 3

---
+++ src/test/Test.java
@@ -0,0 +1,22 @@
+package test;
+
+public class Test {
+    public static void main(String[] args) {
+        B b = new B();
+        b.show();
+
+        A a = new A();
+        a.show();
+    }
+}
+class A {
+    void show() {
+        System.out.println("A");
+    }
+}
+
+class B extends A {
+    void  show() {
+        System.out.println("B");
+    }
+}
