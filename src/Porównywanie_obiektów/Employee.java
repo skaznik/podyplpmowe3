7dd532005122e47e42399f44556ea7dc0a06d278
skaznik <skaznik@wszib.edu.pl>
2019-12-15 08:48:25 +0100

    zjazd 3 dzień 2

---
+++ src/Porównywanie_obiektów/Employee.java
@@ -0,0 +1,22 @@
+package Porównywanie_obiektów;
+
+public class Employee {
+    String name;
+    int age;
+    int salary;
+
+    public Employee(String name, int age, int salary) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", salary=" + salary +
+                '}';
+    }
+}
--- src/Porównywanie_obiektów/Test.java
+++ src/Porównywanie_obiektów/Test.java
@@ -1,9 +1,17 @@
 package Porównywanie_obiektów;
 
+import java.awt.*;
+
 public class Test {
 
     public static void main(String[] args) {
         Integer i = 1;  //zmiena lokalna musi zostać zainicjowana
         System.out.println(i);
+
+        Point point = new Point(3,4);
+        System.out.println(point);
+
+        Employee employee = new Employee("Kowalski", 34, 4500);
+        System.out.println(employee);
     }
 }
