cbdc70ae6b4e0f53b7797b8bc1ae3abec6cf3bf4
skaznik <skaznik@wszib.edu.pl>
2019-12-15 12:26:19 +0100

    zjazd 3 dzień 2

---
+++ src/modyfikatory/WashingMachineApp.java
@@ -0,0 +1,10 @@
+package modyfikatory;
+
+public class WashingMachineApp {
+    public static void main(String[] args) {
+        WashingMachine washingMachine = new WashingMachine();
+        washingMachine.modeForSensitiveClouthes();
+        washingMachine.showSetting();
+
+    }
+}
