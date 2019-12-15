018ebf7f6a477d004ca28a71f49d2af31dca9753
skaznik <skaznik@wszib.edu.pl>
2019-12-15 08:40:19 +0100

    zjazd 3 dzień 2

---
+++ src/Porównywanie_obiektów/Test.java
@@ -0,0 +1,9 @@
+package Porównywanie_obiektów;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Integer i = 1;  //zmiena lokalna musi zostać zainicjowana
+        System.out.println(i);
+    }
+}
