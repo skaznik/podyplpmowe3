5d83c05e6acbce9db19618023f50ace2823932f3
skaznik <skaznik@wszib.edu.pl>
2019-12-15 15:13:59 +0100

    zjazd 3 dzień 2

--- src/Lab14/AnimalShelter.java
+++ src/Lab14/AnimalShelter.java
@@ -4,26 +4,26 @@ import java.util.Random;
 
 public class AnimalShelter {
     public static void main(String[] args) {
-        Animal[] kojce = new Animal[100];
+        Animal[] box = new Animal[100];
         Random random = new Random();
         for (int i = 0; i < 100; i++) {
             int next = random.nextInt(3);
             switch (next) {
                 case 0:
-                    kojce[i] = new Dog();
+                    box[i] = new Dog();
                     break;
                 case 1:
-                    kojce[i] = new Cat();
+                    box[i] = new Cat();
                     break;
                 case 2:
-                    kojce[i] = new Mouse();
+                    box[i] = new Mouse();
                     break;
             }
 
         }
 
-        System.out.println(kojce[33].getType());
-        System.out.println("Liczba " + kojce[33].getType() + " jest " + kojce[33].getInstanceNumber());
+        System.out.println(box[33].getType());
+        System.out.println("Liczba zwierząt rodzaju (" + box[33].getType() + ") w schronisku  wynosi " + box[33].getInstanceNumber());
 
     }
 
