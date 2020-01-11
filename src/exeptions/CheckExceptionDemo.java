64047194e8e7288e64c26d212d34f574e5157a83
skaznik <skaznik@wszib.edu.pl>
2020-01-11 16:15:10 +0100

    zjazd 4 dzień 1

---
+++ src/exeptions/CheckExceptionDemo.java
@@ -0,0 +1,24 @@
+package exeptions;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class CheckExceptionDemo {
+    public static void main(String[] args) {
+        CheckExceptionDemo ced = new CheckExceptionDemo();
+        ced.urlTesterByTryCatch("https://wszib.edu.pl");
+
+    }
+
+    public void urlTesterByTryCatch(String urlStr) {
+        try {
+            URL url = new URL(urlStr);
+            System.out.println("Protocol : " + url.getProtocol());
+            System.out.println("Host : " + url.getHost());
+        } catch (MalformedURLException e) {
+            //e.printStackTrace();
+            System.out.println("Błąd ! : " + e.getMessage());
+        }
+    }
+
+}
