f16ea939fd6b6b528db5fe66a32585daf77140c2
skaznik <skaznik@wszib.edu.pl>
2020-01-11 10:25:22 +0100

    zjazd 4 dzień 1

---
+++ src/Rectangle.java
@@ -0,0 +1,27 @@
+public class Rectangle {
+    public int dlugosc;
+    public int szerokosc;
+
+    public Rectangle() {
+
+    }
+
+    public Rectangle(int dlugosc) {
+        this.dlugosc = dlugosc;
+    }
+
+    public Rectangle(int dlugosc, int szerokosc) {
+        this.dlugosc = dlugosc;
+        this.szerokosc = szerokosc;
+    }
+    
+
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+        Rectangle rectangle1 = new Rectangle(5,7);
+        Rectangle rectangle2 = new Rectangle(5);
+        System.out.println(rectangle);
+        System.out.println(rectangle1);
+        System.out.println(rectangle2);
+    }
+}
