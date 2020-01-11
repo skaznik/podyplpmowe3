ee59420c93bdeabe6ce7072eb5e229cbf8081f94
skaznik <skaznik@wszib.edu.pl>
2020-01-11 12:37:18 +0100

    zjazd 4 dzień 1

--- src/pl/edu/wszib/jwe/LettersApp.java
+++ src/pl/edu/wszib/jwe/LettersApp.java
@@ -1,4 +1,42 @@
 package pl.edu.wszib.jwe;
 
 public class LettersApp {
+    public static void main(String[] args) {
+        A a = new A();
+        a.introduce();;
+        B b = new B();
+        b.introduce();
+        System.out.println("Po interface");
+
+        Letter a1 = new A();
+        a1.introduce();
+
+        Letter b1 = new B();
+        b1.introduce();
+
+        System.out.println("Inny przykład");
+
+        A aaa = new B();
+        aaa.introduce();
+    }
+
 }
+
+interface Letter {
+    void introduce();
+}
+
+class A implements Letter {
+    @Override
+    public void introduce() {
+        System.out.println("Jestem A");
+    }
+}
+
+class B extends A implements Letter {
+    @Override
+    public void introduce() {
+        System.out.println("Jestem B");
+    }
+}
+
