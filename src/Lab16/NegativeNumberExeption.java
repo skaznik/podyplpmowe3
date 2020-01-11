1222e56cc7f122044de010875421160ceb0df5d5
skaznik <skaznik@wszib.edu.pl>
2020-01-11 16:41:10 +0100

    zjazd 4 dzień 1

---
+++ src/Lab16/NegativeNumberExeption.java
@@ -0,0 +1,12 @@
+package Lab16;
+
+public class NegativeNumberExeption {
+
+    public static void main(String[] args) throws NegativeNumberException {
+        NegativeNumberExeption nne = new NegativeNumberExeption();
+        PrimeFinder primeFinder = new PrimeFinder(-2);
+
+    }
+
+
+}
