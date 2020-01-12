be5d735e754f6acdedc31bf83e16ed1e59335e83
skaznik <skaznik@wszib.edu.pl>
2020-01-12 09:37:13 +0100

    zjazd 4 dzień 2

--- src/Lab17/DirectionsTest.java
+++ src/Lab17/DirectionsTest.java
@@ -5,6 +5,8 @@ public class DirectionsTest {
 
     public static void main(String[] args) {
         System.out.println(Directions.EASTH.getPolName());
+        System.out.println(Directions.NORTHandEASTH.getEngName());
+        System.out.println(Directions.NORTHandWEASTH.getsName());
     }
 
 }
